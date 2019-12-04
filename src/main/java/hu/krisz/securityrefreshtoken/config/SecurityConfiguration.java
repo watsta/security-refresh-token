6b3984a024b051f9223dce845dd0eb2525888781
Krisztian Toth <krisztian.toth@cgi.com>
2019-12-04 12:49:58 +0100

    init commit

---
+++ src/main/java/hu/krisz/securityrefreshtoken/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package hu.krisz.securityrefreshtoken.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests ->
+                requests.anyRequest().permitAll()
+        );
+    }
+}
