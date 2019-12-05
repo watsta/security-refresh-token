2898bb3c332007a49dbd9bdde69a92e5e046e839
Krisztian Toth <krisztian.toth@cgi.com>
2019-12-05 10:18:06 +0100

    add test to get random resource with acquired token

--- src/test/java/hu/krisz/securityrefreshtoken/LoginTest.java
+++ src/test/java/hu/krisz/securityrefreshtoken/LoginTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,8 +19,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -43,18 +43,7 @@ public class LoginTest {
 
     @Test
     public void testLogin() throws Exception {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("username", "user");
-        params.add("password", "password");
-
-        var result = mvc.perform(MockMvcRequestBuilders
-                .post("/login")
-                .params(params))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-        var tokenResponse = objectMapper.readValue(result, TokenResponse.class);
+        TokenResponse tokenResponse = login();
 
         assertThat(tokenResponse.getAccessToken(), is(notNullValue()));
         assertTrue(tokenResponse.getExpiresIn() > 0);
@@ -70,5 +59,44 @@ public class LoginTest {
                 .post("/login")
                 .params(params))
                 .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    public void testGetRandomAfterLogin() throws Exception {
+        TokenResponse tokenResponse = login();
+
+        HttpHeaders httpHeaders = createAuthorizationHeader(tokenResponse.getAccessToken());
+        var result = mvc.perform(MockMvcRequestBuilders
+                .get("/random")
+                .headers(httpHeaders))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var randomNumberResponse = objectMapper.readValue(result, RandomNumberResponse.class);
+
+        assertThat(randomNumberResponse.getRandomNumber(), instanceOf(Integer.class));
+    }
+
+    private TokenResponse login() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("username", "user");
+        params.add("password", "password");
+
+        var result = mvc.perform(MockMvcRequestBuilders
+                .post("/login")
+                .params(params))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        return objectMapper.readValue(result, TokenResponse.class);
+    }
+
+    private HttpHeaders createAuthorizationHeader(String accessToken) {
+        MultiValueMap<String, String> headerParams = new LinkedMultiValueMap<>();
+        headerParams.add(HttpHeaders.AUTHORIZATION, accessToken);
+        return new HttpHeaders(headerParams);
     }
 }
