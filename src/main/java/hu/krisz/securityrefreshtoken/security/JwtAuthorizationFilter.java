3d47cdfd280a5477b8b5c774d73961edd1a7d943
Krisztian Toth <krisztian.toth@cgi.com>
2019-12-05 10:04:49 +0100

    log parsing exceptions

--- src/main/java/hu/krisz/securityrefreshtoken/security/JwtAuthorizationFilter.java
+++ src/main/java/hu/krisz/securityrefreshtoken/security/JwtAuthorizationFilter.java
@@ -1,10 +1,14 @@
 package hu.krisz.securityrefreshtoken.security;
 
+import hu.krisz.securityrefreshtoken.security.token.UserTokenInformation;
 import hu.krisz.securityrefreshtoken.security.token.access.AccessTokenService;
+import io.jsonwebtoken.JwtException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,6 +22,8 @@ import java.io.IOException;
  * @author krisztian.toth on 4-12-2019
  */
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthorizationFilter.class);
+
     private final AccessTokenService accessTokenService;
 
     public JwtAuthorizationFilter(AccessTokenService accessTokenService) {
@@ -34,7 +40,15 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             throw new InvalidTokenException("invalid token");
         }
         var token = authHeader.replace("Bearer ", "");
-        var parsedToken = accessTokenService.parse(token);
+
+        UserTokenInformation parsedToken;
+        try {
+            parsedToken = accessTokenService.parse(token);
+        } catch (JwtException e) {
+            LOGGER.info("could not parse token: {}", token, e);
+            throw e;
+        }
+
         var authentication = new UsernamePasswordAuthenticationToken(
                 parsedToken.getUserId(),
                 null,
